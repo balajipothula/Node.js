76d573e6d020be97c7043d8a1407a52523a08277
BALAJI POTHULA <balan.pothula@gmail.com>
2019-04-23 15:07:48 +0530

    Update and rename install-docker.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
 # Date        : 31 August 2016,
-# Description : Installing Docker on Ubuntu.
+# Description : Installing tomcat Docker image on Ubuntu.
 
-# Note: Please run this script with root privilages.
+# Note: Please run this script with sudo privilage.
 
 # uninstalling old versions of docker.
 apt -y remove docker docker-engine docker.io
@@ -13,10 +13,7 @@ apt -y remove docker docker-engine docker.io
 apt update
 
 # upgrading packages.
-apt -y upgrade
-
-# dist upgrading package.
-apt -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
 # installing packages to allow apt to use repository over https.
 apt -y install apt-transport-https \
@@ -27,12 +24,6 @@ apt -y install apt-transport-https \
 # adding docker official gpg key.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-# verifying gpg key.
-#apt-key fingerprint 0EBFCD88
-
-# deleting old docker gpg key(s).
-#apt-key del 2C52609D
-
 # setting up the stable docker repository.
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
@@ -42,6 +33,3 @@ apt update
 # installing latest version of docker ce.
 apt -y install docker-ce docker-ce-cli containerd.io
 
-# running docker as non-sudoer.
-# note: reboot required.
-usermod -a -G docker $USER
